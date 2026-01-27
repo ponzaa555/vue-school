d8679eff350be77e2305cfe13ee9b2b872077487
ponzaa567 <ponpon13173@gmail.com>
2026-01-27 18:09:37 +0700

    - apply active-line style

--- src/router/index.ts
+++ src/router/index.ts
@@ -13,7 +13,8 @@ const routes = [
 
 const router = createRouter({
     history:createWebHistory(),
-    routes
+    routes,
+    linkActiveClass:'vue-school-active-link'
 })
 
 export default router
