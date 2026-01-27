f8258ae684bbbdae0441093f271b1373117c500f
ponzaa567 <ponpon13173@gmail.com>
2026-01-27 22:16:00 +0700

    - dynamic route - compute value

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import Home from "@/views/Home.vue"
+// import path = require("path")
 import { createRouter, createWebHistory } from "vue-router"
 // import path = require("path")
 
@@ -8,7 +9,8 @@ const routes = [
     {path:'/brazil',name:'brazil', component: () =>import('@/views/Brazil.vue')},
     {path:'/hawaii',name:'hawaii', component: () =>import('@/views/Hawaii.vue')},
     {path:'/jamaica',name:'jamaica', component: () =>import('@/views/Jamaica.vue')},
-    {path:'/panama',name:'panama', component: () =>import('@/views/Panama.vue')}
+    {path:'/panama',name:'panama', component: () =>import('@/views/Panama.vue')},
+    {path:'/destinations/:id' , component: () => import('@/views/DestinationShow.vue')}
 ]
 
 const router = createRouter({
