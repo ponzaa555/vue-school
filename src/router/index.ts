2cabc8d825db303f74c819604baead43ff82a34c
ponzaa567 <ponpon13173@gmail.com>
2026-01-27 17:51:43 +0700

    - Add lazy loading index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,14 @@
 import Home from "@/views/Home.vue"
-import About from "@/views/About.vue"
 import { createRouter, createWebHistory } from "vue-router"
 // import path = require("path")
-import Brazil from "@/views/Brazil.vue"
-import Hawaii from "@/views/Hawaii.vue"
-import Jamaica from "@/views/Jamaica.vue"
-import Panama from "@/views/Panama.vue"
 
 
 const routes = [
     {path:'/',name:'Home',component:Home},
-    {path:'/about',name:'About',component:About},
-    {path:'/brazil',name:'brazil', component:Brazil},
-    {path:'/hawaii',name:'hawaii', component:Hawaii},
-    {path:'/jamaica',name:'jamaica', component:Jamaica},
-    {path:'/panama',name:'panama', component:Panama}
+    {path:'/brazil',name:'brazil', component: () =>import('@/views/Brazil.vue')},
+    {path:'/hawaii',name:'hawaii', component: () =>import('@/views/Hawaii.vue')},
+    {path:'/jamaica',name:'jamaica', component: () =>import('@/views/Jamaica.vue')},
+    {path:'/panama',name:'panama', component: () =>import('@/views/Panama.vue')}
 ]
 
 const router = createRouter({
