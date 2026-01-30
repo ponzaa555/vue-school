e9ff9981c7267fbba7a97abb374ae16f31f0434d
ponzaa567 <ponpon13173@gmail.com>
2026-01-31 00:33:13 +0700

    - ใช้ this.$watch เหมือน useEffect() ที่จะ run function ด้วนในถ้าตัวแปลมีการเปลี่ยนแปลง - ใช้ attribute :key เพื่อบอก vue ว่าเป็น component ใหม่ให้ reload

--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="destination">
+    <section class="destination" v-if="destination">
         <h1>{{ destination.name }}</h1>
         <div class="destination-details">
             <img :src="`/images/${destination.image}`" :alt="destination.name"/>
@@ -9,16 +9,33 @@
 </template>
 
 <script>
-import sourceData from "@/data.json"
 export default{
+    data(){
+        return{
+            destination : null
+        }
+    },
     computed:{
         destinationId(){
             return parseInt(this.$route.params.id)
         },
         // ถ้าจะเรียกใช้ ค่าใน Compute ต้องเรียกด้วย this. หรือ ถ้าเรียกใช้ใน template จะเรียกผ่าน {{}}
-        destination(){
-            return sourceData.destinations.find(des => des.id === this.destinationId)
+    },
+    methods:{
+        async initData(){
+            const response = await fetch(`https://travel-dummy-api.netlify.app/${this.$route.params.slug}.json`)
+            this.destination = await response.json()
         }
+    },
+    created(){
+        this.initData();
     }
+    // created() {
+    // this.$watch(
+    //     () => this.$route.params.slug,
+    //     this.initData,
+    //     { immediate: true }
+    //     )
+    // }
 }
 </script>
