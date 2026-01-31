285e7b85231fb37c3508711e45a7b0157bc183d0
ponzaa567 <ponpon13173@gmail.com>
2026-01-31 23:49:28 +0700

    - กำหนด type ให้ router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,37 @@
 import Home from "@/views/Home.vue"
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router"
 // import path = require("path")
 
 
-const routes = [
+const routes : RouteRecordRaw[] = [
     {path:'/',name:'Home',component:Home},
     {
         path:'/destinations/:id/:slug' ,
         name: 'destination.show' , 
         component: () => import('@/views/DestinationShow.vue'),
-        props: route => ({...route.params ,id:parseInt(route.params.id)}),
+        props: (route) => {
+            const idParam = route.params.id
+            const idStr = Array.isArray(idParam) ? idParam[0] : idParam
+          
+            return {
+              ...route.params,
+              id: idStr ? parseInt(idStr, 10) : null
+            }
+          },
         children:[
             {
                 path:':experienceSlug',
                 name:'experience.show',
                 component: () => import('@/views/ExperienceShow.vue'),
-                props: route => ({...route.params,id:parseInt(route.params.id)})
+                props: (route) => {
+                    const idParam = route.params.id
+                    const idStr = Array.isArray(idParam) ? idParam[0] : idParam
+
+                    return {
+                        ...route.params,
+                        id: idStr ? parseInt(idStr, 10) : null
+                    }
+                }
             }
         ]
     }
